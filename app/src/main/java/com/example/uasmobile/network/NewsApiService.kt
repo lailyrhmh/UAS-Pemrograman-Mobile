6ecc57e17e1c4da3d6014ac52ef430a9ff1f129a
Annisa Mahfuzhah <2010817220018@mhs.ulm.ac.id>
2022-06-04 12:41:07 +0800

    menambahkan api service
    Initial commit

---
+++ app/src/main/java/com/example/uasmobile/network/NewsApiService.kt
@@ -0,0 +1,35 @@
+package com.example.uasmobile.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+const val BASE_URL = "https://berita-indo-api.vercel.app/"
+
+val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NewsApiService{
+    @GET(value = "https://berita-indo-api.vercel.app/v1/cnn-news/nasional")
+    suspend fun getNasional() : List<Nasional>
+
+    @GET(value = "https://berita-indo-api.vercel.app/v1/cnn-news/internasional")
+    suspend fun getInternasional() : List<Interasional>
+
+    @GET(value = "https://berita-indo-api.vercel.app/v1/cnn-news/olahraga")
+    suspend fun getOlahrahga() : List<Olahraga>
+}
+
+object NewsApi{
+    val retrofitService : NewsApiService by lazy {
+        retrofit.create(NewsApiService::class.java)
+    }
+}
