8cc3dbb559dc3daac498e5e37b2f79fd67ab9b22
lailyrhmh <lailyrhmh11@gmail.com>
2022-06-05 16:52:40 +0800

    fix menjalankan tampilan navbar

--- app/src/main/java/com/example/uasmobile/MainActivity.kt
+++ app/src/main/java/com/example/uasmobile/MainActivity.kt
@@ -25,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_view) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
